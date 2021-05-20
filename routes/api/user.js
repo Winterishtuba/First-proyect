04a9d95bf24af0e155c18351493b4e34de73242a
Winterishtuba <winterishtuba@gmail.com>
2021-05-20 16:49:51 -0300

    profe no se crea era chiste

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const User = require('../../models/User');
+const User = require('../../models/user.js');
 
 router.get('/users', (req, res) => {
     User.find((err, users) => {
